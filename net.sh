416661bef78abe31f3992c78b74d0bd90d9b855a
niladridatta <niladridatta@gmail.com>
2014-06-07 22:36:22 +0530

    Added ignore contracts

--- net.sh
+++ net.sh
@@ -1,4 +1,4 @@
-cat contracts.txt | egrep 'CE|PE' | awk '{print $3 $1, $7}' > temp.txt
+cat contracts.txt | grep -v "#" | egrep 'CE|PE' | awk '{print $3 $1, $7}' > temp.txt
 
 if [ $# == 2 ]; then 
 	echo Range: $1 .. $2
