ca734386e5101a6d72fc36ca50c4534c3149863e
niladridatta <niladridatta@gmail.com>
2014-08-14 23:46:14 +0530

    Added pos calculations

--- sum.pl
+++ sum.pl
@@ -4,11 +4,14 @@ my $file = "data.txt";
 open(FILE, $file) or die("Unable to open file: $file\n");
 
 my @data, $val, $tot = 0;
+my @output = ("\nToday:\n", "\nPrev:\n", "\nMin:\n");
 
 while (<FILE>) {
-
+  
   @data = split(' ');
-  print "@data\n\n";
+  
+  print "$output[$i++]\n";
+  print "@data\n";
   
   foreach my $val (@data) {
 	$tot += $val;
--- total.sh
+++ total.sh
@@ -1,3 +1,7 @@
 cat today.txt  | awk '{print $11 }' | xargs > data.txt
+head -1 prev.txt >> data.txt
+
+echo >> data.txt
+sh run.sh  | awk '{print $4}'| sort -n | head -1 >> data.txt
 
 perl sum.pl
