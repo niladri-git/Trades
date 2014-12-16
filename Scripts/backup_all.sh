41daad32bf6e68dc9f9221eab59f8780f72018d2
niladridatta <niladridatta@gmail.com>
2014-12-16 15:49:55 +0530

    Updated 16-Dec and backup all script

---
+++ Scripts/backup_all.sh
@@ -0,0 +1,11 @@
+sh backup.sh
+echo
+
+sh backup_AR.sh
+echo
+
+sh backup_TJ.sh
+echo
+
+sh backup_far.sh
+echo
---
+++ Scripts/total_AR.sh
@@ -0,0 +1,13 @@
+cat today.txt  | grep -v FUT | awk '{print $11 }' | xargs > data.txt
+head -1 prev.txt >> data.txt
+
+perl sum.pl
+
+if [ $# -eq 1 ]
+then
+  perl sum.pl | tail -1 | awk '{print $2}' > prev.txt.new
+  exit
+else
+  echo
+  sh run.sh 
+fi
