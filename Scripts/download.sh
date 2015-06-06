12d1ce291af14b65d3cb47026b36adbe5a01a854
Niladri Datta <dattaniladri@gmail.com>
2015-06-06 03:38:55 -0700

    Updated new box 06-06-2015

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,3 +1,5 @@
 #Downloads the file from Dropbox
 
-cp -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/*.txt .
+#cp -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/*.txt .
+
+cp -vf /c/Users/niladri/Dropbox/Notes/*.txt .
--- Scripts/upload.sh
+++ Scripts/upload.sh
@@ -1,8 +1,21 @@
 #Uploads the file to Dropbox
 
-cp contracts.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
-cp AR.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
-cp TJ.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
-cp RK.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
-cp far.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
-cp projected.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
+dir="/c/Users/niladri/Dropbox/Notes/"
+
+#cp contracts.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
+cp contracts.txt -vf $dir
+
+#cp AR.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
+cp AR.txt -vf $dir
+
+#cp TJ.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
+cp TJ.txt -vf $dir
+
+#cp RK.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
+cp RK.txt -vf $dir
+
+#cp far.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
+cp far.txt -vf $dir
+
+#cp projected.txt -vf /c/Documents\ and\ Settings/niladridatta/My\ Documents/Dropbox/Notes/
+cp projected.txt -vf $dir
